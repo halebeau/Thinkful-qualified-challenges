4e4e7634300b29e530a508b8713951aecaa8c345
Beau Hale <halebeau3@gmail.com>
2021-10-05 17:07:07 -0700

    Fixed: front-end 2nd attempt code

--- MOCK/frontend-2nd_attempt/AlbumDetail.js
+++ MOCK/frontend-2nd_attempt/AlbumDetail.js
@@ -1,30 +1,28 @@
 import React, { useState, useEffect } from "react";
 
 export default function AlbumDetail({ album, id }) {
-const [photos, setPhotos] = useState([]);
-useEffect(() => {
-   fetch(`https://jsonplaceholder.typicode.com/albums/${album.id}/photos`)
-      .then((response) => response.json())
-      .then(setPhotos)
-      .catch((error) => {
-      console.log(error);
-      });
-}, []);
+   const [photos, setPhotos] = useState([]);
+   useEffect(() => {
+      fetch(`https://jsonplaceholder.typicode.com/albums/${album.id}/photos`)
+         .then((response) => response.json())
+         .then(setPhotos)
+         .catch((error) => {
+         console.log(error);
+         });
+   }, []);
 
-const photoMap = photos.map((photo, index) => {
-   return (
-      <div key={index}>
-      {id === album.id ? (
-         <div>
-            <p>{photo.title}</p>
-            <img src={photo.thumbnailUrl} alt={photo.title} />
+   const photoMap = photos.map((photo, index) => {
+      return (
+         <div key={index}>
+         {id === album.id ?
+            <div>
+               <p>{photo.title}</p>
+               <img src={photo.thumbnailUrl} alt={photo.title} />
+            </div>
+         : <p></p>}
          </div>
-      ) : (
-         <p></p>
-      )}
-      </div>
-   );
-});
+      );
+   });
 
-return <p>{photoMap.slice(0, 10)}</p>;
+   return <p>{photoMap.slice(0, 10)}</p>;
 }
---
+++ MOCK/frontend-2nd_attempt/practice.js
@@ -0,0 +1,44 @@
+import React, { UseState, useEffect } from 'react';
+
+function ProfileEdit() {
+   const [user, setUser] = useState({});
+
+   function changeHandler(event) {
+      setUser({ ...user, [event.target.name]: event.target.value 
+      });
+   }
+
+   useEffect(() => {
+      async function loadUsers() {
+         const response = await fetch(
+            "https://jsonplaceholder.typicode.com/users/1"
+         );
+         const userFromAPI = await reponse.json();
+         setUser(userFromAPI);
+      }
+      loadUsers();
+   }, []);
+
+   if (user.id) {
+      return (
+         <form name="profileEdit">
+            <div>
+               <label htmlFor="username">User Name:</label>
+               <input
+                  id="username"
+                  name="username"
+                  type="text"
+                  value={user.username}
+               />
+            </div>
+            <div>
+               <label htmlFor="email">Email:</label>
+               <input id="email" name="email" type="email" value={user.email} />
+            </div>
+         </form>
+      );
+   } 
+   return "Loading...";  
+}
+
+export default ProfileEdit;
