9e795f365bd86fa0dd462cff5be43cdc35a9626d
Beau Hale <halebeau3@gmail.com>
2021-10-07 13:33:46 -0700

    Change:  optimal refactor solution

--- MOCK/frontend-2nd_attempt/Test 1/Solution 2/App2.js
+++ MOCK/frontend-2nd_attempt/Test 1/Solution 2/App2.js
@@ -28,3 +28,30 @@ function App() {
 }
 
 export default App;
+
+
+//REFACTOR
+
+import React, { useEffect, useState } from "react";
+import AlbumDetail from "./AlbumDetail";
+
+function App() {
+   const [albums, setAlbums] = useState([]);
+
+   useEffect(() => {
+      fetch("https://jsonplaceholder.typicode.com/albums?userId=1")
+         .then((response) => response.json())
+         .then(setAlbums)
+         .catch((error) => {
+         console.log(error);
+         });
+   }, []);
+
+   return (
+      <div className="App">
+         {albums.map((album) => (<AlbumDetail key={album.id} {...album} />))}
+      </div>
+   );
+}
+
+export default App;
